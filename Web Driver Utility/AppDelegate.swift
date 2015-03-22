ea8caebe97f4c8c8d9fa9868db49af5977bc3084
Michal Olszewski <olszewski.m.l@gmail.com>
2015-03-22 22:40:47 +0100

    Initial Commit

---
+++ Web Driver Utility/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Web Driver Utility
+//
+//  Created by Michal Olszewski on 22.03.2015.
+//  Copyright (c) 2015 Olszewski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Web Driver Utility/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Web Driver Utility
+//
+//  Created by Michal Olszewski on 22.03.2015.
+//  Copyright (c) 2015 Olszewski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
